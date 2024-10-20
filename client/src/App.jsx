b6213eb0b4dcba4337de917a0665c66feefe72e0
PKTam <tampham11072003@gmail.com>
2024-10-20 22:43:03 +0700

    ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
+
+import { useAuth } from 'hooks/useAuth';
+import { useAuthContext } from 'hooks/useAuthContext';
+import { useNoteContext } from 'hooks/useNoteContext';
+
+import Layout from './components/Layout';
+import ProtectedRoutes from './components/ProtectedRoutes';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import Note from './pages/Note';
+import Home from './pages/Home';
+
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loadUser } = useAuth();
+  const { authIsReady } = useAuthContext();
+  const { notesAreReady } = useNoteContext();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    (async () => {
+      await loadUser();
+    })();
+  }, []);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+
+        <Route element={<ProtectedRoutes needAuth />}>
+          <Route path="/" element={notesAreReady && <Layout />}>
+            <Route path="/notes/getting-started" element={<Home />} />
+            <Route path="/notes/:noteId" element={<Note />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </>
+    )
+  );
+
+  return <>{authIsReady && <RouterProvider router={router} />}</>;
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './config/db.js';
-import { handleError } from './middlewares/handleError.js';
-import notesRouter from './routes/notes.js';
-import userRouter from './routes/user.js';
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import { handleError } from "./middlewares/handleError.js";
+import userRouter from "./routes/user.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/notes', notesRouter);
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
 
 app.use(handleError);
 
